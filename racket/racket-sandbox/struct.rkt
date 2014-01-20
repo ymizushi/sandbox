3b0eb5bc7677e8e72411d0a29fdba3f07e1a11fc
ymizushi <mizushi@gmail.com>
2014-01-21 02:34:13 +0900

    edit code

--- racket/racket-sandbox/struct.rkt
+++ racket/racket-sandbox/struct.rkt
@@ -6,20 +6,22 @@
   #:auto-value 0
   #:transparent)
 
-(struct figuri (x y [z #:auto])
-  #:auto-value 0
-  #:transparent)
-
 (struct mood-procedure (base rating)
     #:property prop:procedure (struct-field-index base))
 (define happy+ (mood-procedure add1 10))
 (happy+ 2)
+
 (mood-procedure-rating happy+)
 
 (figure? (figure 1 2))
-(figuri? (figuri 1 2))
 
-;(figure-y ())
+(struct square (x y width height))
+(define sq1 (square 1 2 3 4))
+
+(check-expect (square-x sq1) 1)
+(check-expect (square-y sq1) 2)
+(check-expect (square-width sq1) 3)
+(check-expect (square-height sq1) 4)
 
 (check-expect (+ 1 1) 2)
 (test)
