c31c6a59c275f7f16b55b68dee9903dd6e2fde32
ymizushi <mizushi@gmail.com>
2014-06-14 03:01:18 +0900

    edit code

--- javascript/emojimaker/main.js
+++ javascript/emojimaker/main.js
@@ -1,48 +1,86 @@
-function moveImg() {
-  pelement = document.getElementById("p_element");
-
-  // document.body.p.appendChild(img);
-}
-
-function widthChange() {
-  widthNumberButton = document.getElementById("width_button");
-  widthNumber = widthNumberButton.value;
-}
-
 function heightChange() {
   heightNumberButton = document.getElementById("height_button");
   heightNumberButton.value
   heightNumber = heightNumberButton.value;
 }
 
-function createImg() {
-  var img = document.createElement("img")
+function createBaseImg(x, y) {
+  var img = document.createElement("img");
   img.src = "https://a248.e.akamai.net/assets.github.com/images/icons/emoji/arrow_up.png"; 
-  img.class = "emoji"; 
-  img.title = ":arrow_up:"; 
+  img.id= x + "_" + y + "_" + "emoji";
   img.alt=":arrow_up:";
+  img.class = "emoji"; 
+  img.draggable="true";
   img.height="20";
   img.width="20";
   img.align="absmiddle";
+  img.setAttribute("ondragstart", "f_dragstart(event)");
   return img;
+}
+
+function createImg(x, y) {
+  var img = document.createElement("img");
+  img.src = "https://a248.e.akamai.net/assets.github.com/images/icons/emoji/arrow_down.png"; 
+  img.id= x + "_" + y + "_" + "emoji";
+  img.alt=":arrow_down:";
+  img.class = "emoji"; 
+  img.draggable="true";
+  img.height="20";
+  img.width="20";
+  img.align="absmiddle";
+  img.setAttribute("ondragstart", "f_dragstart(event)");
+  return img;
+}
+
+function createDropboxDiv(id) {
+  var div = document.createElement("span");
+  div.id = id;
+  div.setAttribute("ondragover", "f_dragover(event)");
+  div.setAttribute("ondrop", "f_drop(event)");
+  return div
 }
 
 function generate() {
   width = document.getElementById("width_button").value;
   height = document.getElementById("height_button").value;
   pElement = document.getElementById("p_element");
-  console.log(pElement);
-
-  img = createImg();
 
   for (y=0;y<height;y++) {
-    for(x=0;x<width;x++) {
-      var img = createImg();
-      pElement.appendChild(img);
+    for (x=0;x<width;x++) {
+      var div = createDropboxDiv(x+"_"+y+"_"+"dropbox")
+      var img = createBaseImg(x, y);
+      div.appendChild(img);
+      pElement.appendChild(div);
     }
     var br = document.createElement("br")
     pElement.appendChild(br);
   }
+
+  dropbox = document.getElementById("dropbox");
+  for (y=0;y<height;y++) {
+    for (x=0;x<width;x++) {
+      var img = createImg(x, y);
+      dropbox.appendChild(img);
+    }
+    var br = document.createElement("br")
+    dropbox.appendChild(br);
+  }
+
 }
 
-window.document.onclick = moveImg;
+
+function f_dragstart(event){
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function f_dragover(event){
+  event.preventDefault();
+}
+
+function f_drop(event){
+  var id_name = event.dataTransfer.getData("text");
+  console.log(id_name);
+  var drag_elm =document.getElementById(id_name);
+  event.currentTarget.appendChild(drag_elm);
+  event.preventDefault();
+}
