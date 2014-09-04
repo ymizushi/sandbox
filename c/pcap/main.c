d96c3becb43aa6c2ef0dc3922ceb0a0b3462246f
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2014-09-04 20:50:49 +0900

    edit main.c

--- c/pcap/main.c
+++ c/pcap/main.c
@@ -53,28 +53,53 @@ int run() {
 }
 
 int main(int argc, char *argv[]) {
-    char *dev, errbuf[PCAP_ERRBUF_SIZE];
+    char *dev;
+    pcap_t *handle;      /* Session handle */
+    char errbuf[PCAP_ERRBUF_SIZE];   /* Error string */
+    struct bpf_program fp;      /* The compiled filter expression */
+    char filter_exp[] = "port 80";   /* The filter expression */
+    bpf_u_int32 mask =0;      /* The netmask of our sniffing device */
+    bpf_u_int32 net = 0;      /* The IP of our sniffing device */
+
+    struct pcap_pkthdr header; /* The header that pcap gives us */
+    const u_char *packet; /* The actual packet */
 
     dev = pcap_lookupdev(errbuf);
+    dev = "en1";
     if (dev == NULL) {
         fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
         return(2);
     }
-
-    pcap_t *handle;
-
+    
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+        fprintf(stderr, "Can't get netmask for device %s\n", dev);
+        fprintf(stderr, "errbuf %s\n", errbuf);
+        net = 0;
+        mask = 0;
+    }
     handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         return(2);
     }
-    if (pcap_datalink(handle) != DLT_EN10MB) {
-        fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", dev);
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return(2);
+    }
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return(2);
     }
 
-    printf("Device: %s\n", dev);
-    run();
+    /* Grab a packet */
+    packet = pcap_next(handle, &header);
+    /* Print its length */
+    printf("Jacked a packet with length of [%d]\n", header.len);
+    /* And close the session */
+    pcap_close(handle);
+    return(0);
 
+
+    run();
     return(0);
 }
