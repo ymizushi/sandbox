11a54eade99397f67b985ece5aaecdb4fe7d7061
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2014-09-04 16:45:35 +0900

    main.c

--- c/pcap/main.c
+++ c/pcap/main.c
@@ -55,12 +55,26 @@ int run() {
 int main(int argc, char *argv[]) {
     char *dev, errbuf[PCAP_ERRBUF_SIZE];
 
-    run();
     dev = pcap_lookupdev(errbuf);
     if (dev == NULL) {
         fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
         return(2);
     }
+
+    pcap_t *handle;
+
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return(2);
+    }
+    if (pcap_datalink(handle) != DLT_EN10MB) {
+        fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", dev);
+        return(2);
+    }
+
     printf("Device: %s\n", dev);
+    run();
+
     return(0);
 }
