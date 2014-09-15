9fe44edfde741817c19222d054723f1d8b2136a5
ymizushi <mizushi@gmail.com>
2014-09-15 16:38:39 +0900

    add erlang file

--- erlang/hello.erl
+++ erlang/hello.erl
@@ -1,5 +1,13 @@
 -module(hello).
 -export([start/0]).
+-export([fib/1]).
 
 start() ->
-  io:format("~s~n", ["Hello, Erlang!"]).
+  io:format("~p~n", [fib(10)]).
+
+fib(N) when N =:= 0 ->
+    1;
+fib(N) when N =:= 1 ->
+    1;
+fib(N) when 2 =< N ->
+    fib(N-2) + fib(N-1).
