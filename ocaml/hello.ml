8a46ffe0627422a350ec197f618dd6013583a2de
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2014-10-29 18:18:05 +0900

    edit code

--- ocaml/hello.ml
+++ ocaml/hello.ml
@@ -1,6 +1,29 @@
+(*
 let rec quicksort = function
   | [] -> []
   | pivot :: rest ->
       let is_less x = x < pivot in
       let left, right = List.partition is_less rest in
       quicksort left @ [pivot] @ quicksort right
+*)
+
+(*
+        let f x = x + 1 in
+          print_int(f (f 3));;
+    *)
+
+
+let rec gcd x y = 
+  if x = y then
+    x
+  else
+    if x > y then
+      gcd (x-y) y
+    else
+      gcd x (y-x)
+
+let hoge x = 
+  x
+
+let _ =
+  print_int (gcd 5 15);;
