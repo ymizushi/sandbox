41f6499fbc223237cd81b9f40fbb307debc04e1a
Yuta Mizushima <yuta_mizushima@dwango.co.jp>
2014-10-31 19:00:05 +0900

    edit code

--- clojure/test.clj
+++ clojure/test.clj
@@ -1,10 +1,30 @@
 (use 'clojure.test)
 
-(defn hoge [] (+ 1 2))
+(defprotocol Evalable
+  (evl [this]))
+(defprotocol Findable
+  (fnd [dict key]))
 
-(deftest test-add []
-  (testing "add"
-    (is (= 2 (+ 1 1)))
-    (is (= 3 (hoge)))
-    )) 
+(deftype Token [s])
+(deftype Atom [token])
+(deftype Int [token]
+  Evalable
+  (evl [this]
+    (int token)))
+
+(deftype Env [dic outer]
+  Evalable
+  Findable
+  (fnd [_ key]
+    (if-let [value (get dic key)]
+      value
+      (if-let [value (fnd outer key)]
+        value
+        nil))))
+
+(deftest test-Env []
+  (testing "fnd"
+    (is (= (fnd (Env. {:hoge (Token. "1")} nil) :hoge)
+           (Token. "1"))))) 
+
 (run-tests)
