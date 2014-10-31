6b66b47580eded19a993d38a4957e29e82255476
ymizushi <mizushi@gmail.com>
2014-11-01 04:02:19 +0900

    edit code

---
+++ clojure/repl.clj
@@ -0,0 +1,20 @@
+(defprotocol Evalable
+  (evaluate [_ env]))
+
+(defprotocol Drawable
+  (draw [_ env]))
+
+(deftype Symbol [token])
+
+(extend-type Symbol
+  Drawable
+  (draw [_ env] (print "hoge"))
+  Evalable
+  (evaluate [_ env]
+    (println (+ 1 1))))
+
+(extend-type Symbol
+  Drawable
+    (draw [_ env] (print "piyo")))
+
+(draw (Symbol. "hoge") "hoge")
---
+++ clojure/repl/test/repl/core_test.clj
@@ -0,0 +1,7 @@
+(ns repl.core-test
+  (:require [clojure.test :refer :all]
+            [repl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
