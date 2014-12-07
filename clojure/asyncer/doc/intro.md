928dc99bc7ae5c5882e9cb6f509051c997d0a7b8
ymizushi <mizushi@gmail.com>
2014-12-07 20:47:10 +0900

    edit code

---
+++ clojure/asyncer/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to asyncer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/asyncer/test/asyncer/core_test.clj
@@ -0,0 +1,7 @@
+(ns asyncer.core-test
+  (:require [clojure.test :refer :all]
+            [asyncer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
