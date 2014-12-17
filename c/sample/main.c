0ed474ab2aebdc5509fd6edaa63b727f6f5eb414
ymizushi <mizushi@gmail.com>
2014-12-18 03:32:52 +0900

    edit code

--- c/sample/main.c
+++ c/sample/main.c
@@ -1,46 +1,26 @@
-#if 0
-#ifndef lint
-static char sccsid[] = "@(#)util.c      8.3 (Berkeley) 4/2/94";
-#endif /* not lint */
-#endif
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
-
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#include <ctype.h>
-#include <err.h>
-#include <fts.h>
-#include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <wchar.h>
-#include <wctype.h>
 
-// #include "ls.h"
-// #include "extern.h"
+int plus(int, int);
+int exec_callback(int (*)(int,int));
 
 int
-prn_normal(const char *s)
+main(void)
 {
-    mbstate_t mbs;
-    wchar_t wc;
-    int i,n;
-    size_t clen;
+    int (*funcpointer)(int,int) = plus;
+// exec_callback(&funcpointer);
+    printf("hoge%d\n", funcpointer(5,3));
+}
 
-    memset(&mbs, 0, sizeof(mbs));
-    n = 0;
-    while ((clen = mbrtowc(&wc, s, MB_LEN_MAX, &mbs)) != 0) {
-        if (clen == (size_t)-2) {
-            n += printf("%s", s);
-            break;
-        }
-    
-    }
-    if (clen == (size_t)-1) {
-        membset(&mbs ,0 ,sizeof(mbs));
-        s++;
-    }
+
+int
+plus(int x,int y)
+{
+    return x + y;
+}
+
+
+int
+exec_callback(int (*funcpointer)(int,int))
+{
+    return funcpointer(5,3);
 }
