ccfd5ab2287c2f3d878c33b5d4bde63dac76f499
ymizushi <mizushi@gmail.com>
2015-07-26 19:11:17 +0900

    edit code

---
+++ go/a_tour_of_go/Ex44.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+    i := 0
+    var fib_func func(int) int
+    fib_func = func(n int) int {
+        if n < 2 {
+            return 1
+        }
+        return fib_func(n-2)+fib_func(n-1)
+    }
+    return func() int {
+        i += 1
+        return fib_func(i)
+    }
+// return fib(n-2)+fib(n-1)
+}
+
+func main() {
+    f := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
+}
