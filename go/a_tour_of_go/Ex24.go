c0d550f75634dc22f25cd54215e0663c79d1486b
ymizushi <mizushi@gmail.com>
2015-07-26 16:43:04 +0900

    edit code

--- go/a_tour_of_go/Ex24.go
+++ go/a_tour_of_go/Ex24.go
@@ -2,11 +2,27 @@ package main
 
 import (
     "fmt"
+    "math"
 )
 
-func Sqrt(x float64) float64 {
+func Newton(z float64, x float64) float64 {
+    result := z-((z*z-x)/(2*z))
+    return result
 }
 
+func Sqrt(x float64) float64 {
+    z := Newton(1.0, x)
+    for i:=0;i<10;i++  {
+        z = Newton(z, x)
+        fmt.Println(z)
+    }
+    return z
+}
+
+
 func main() {
-    fmt.Println(Sqrt(2))
+    fmt.Println("my impl:")
+    fmt.Println(Sqrt(3))
+    fmt.Println("old impl:")
+    fmt.Println(math.Sqrt(3))
 }
