1648334465899856a6cf7fc2d8edd7a7eaf4d8f4
ymizushi <mizushi@gmail.com>
2015-08-07 01:57:39 +0900

    edit code

--- go/a_tour_of_go/Ex61.go
+++ go/a_tour_of_go/Ex61.go
@@ -4,11 +4,9 @@ import (
     "io"
     "os"
     "strings"
+    "fmt"
 )
 
-type rot13Reader struct {
-    r io.Reader
-}
 
 type MyError struct {
     message string
@@ -18,13 +16,80 @@ func (self MyError) Error() string {
    return  self.message
 }
 
-func (r rot13Reader) Read(p []byte) (n int, err error) {
-    return 1, MyError{"hoge"}
+type rot13Reader struct {
+    r io.Reader
+}
+
+func (self rot13Reader) Read(p []byte) (int, error) {
+    // alphabetMap := map[string]string{
+    //    "a":"n",
+    //    "b":"o",
+    //    "c":"p",
+    //    "d":"q",
+    //    "e":"r",
+    //    "f":"s",
+    //    "g":"t",
+    //    "h":"u",
+    //    "i":"v",
+    //    "j":"w",
+    //    "k":"x",
+    //    "l":"y",
+    //    "m":"z",
+    //    "n":"a",
+    //    "o":"b",
+    //    "p":"c",
+    //    "q":"d",
+    //    "r":"e",
+    //    "s":"f",
+    //    "t":"g",
+    //    "u":"h",
+    //    "v":"i",
+    //    "w":"j",
+    //    "x":"k",
+    //    "y":"l",
+    //    "z":"m",
+    //    "A":"N",
+    //    "B":"O",
+    //    "C":"P",
+    //    "D":"Q",
+    //    "E":"R",
+    //    "F":"S",
+    //    "G":"T",
+    //    "H":"U",
+    //    "I":"V",
+    //    "J":"W",
+    //    "K":"X",
+    //    "L":"Y",
+    //    "M":"Z",
+    //    "N":"A",
+    //    "O":"B",
+    //    "P":"C",
+    //    "Q":"D",
+    //    "R":"E",
+    //    "S":"F",
+    //    "T":"G",
+    //    "U":"H",
+    //    "V":"I",
+    //    "W":"J",
+    //    "X":"K",
+    //    "Y":"L",
+    //    "Z":"M",
+    // }
+
+    for i:=0;i<len(p);i=i+1 {
+        p[i] = p[i] + 14
+    }
+    return self.r.Read(p)
 }
 
 func main() {
-    s := strings.NewReader(
-        "Lbh penpxrq gur pbqr!")
+    s := strings.NewReader("Lbh penpxrq gur pbqr!")
     r := rot13Reader{s}
-    io.Copy(os.Stdout, &r)
+    result, err := io.Copy(os.Stdout, r)
+    if err == nil {
+        fmt.Println(result)
+    } else {
+        panic(err)
+
+    }
 }
