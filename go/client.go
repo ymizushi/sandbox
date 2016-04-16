c8a3b1fd408da23472741271f85b69b73d139357
ymizushi <mizushi@gmail.com>
2016-04-16 16:05:13 +0900

    rename file

---
+++ go/client.go
@@ -0,0 +1,14 @@
+package client
+import "net"
+import "fmt"
+import "bufio"
+
+func main() {
+    conn, err := net.Dial("tcp", "localhost:8000")
+    if err != nil {
+        fmt.Fprintf(conn, "error GET / HTTP/1.0\r\n\r\n")
+    }
+    fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+    status, err := bufio.NewReader(conn).ReadString('\n')
+    fmt.Printf(status)
+}
--- go/hoge.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func bench() int {
-	return 0
-}
-
-func main() {
-
-}
