3b8d5e25d9f360f3814646f5cc2cacc7b66ed827
ymizushi <mizushi@gmail.com>
2016-07-27 04:00:20 +0900

    Edit code

--- go/gomi/gomi/template/parser.go
+++ go/gomi/gomi/template/parser.go
@@ -1,9 +1,29 @@
 package template
 
-type Parser struct {
 
+interface Parser {
 }
 
-func (p *Parser) Parse(bytes []byte) {
+type State struct {
+    State int
+}
+
+const (
+    INIT = iota
+    START_BRACKET
+    CONTENT
+    END_BRACKET
+    GOAL
+)
+
+const (
+    START_BRACKET_CHARACTER = 123
+    END_BRACKET_CHARACTER = 125
+)
+
+func (p *Parser) Parse(c byte, params map[string]string) []byte {
+    swtich c {
+        case START_BRACKET_CHARACTER:
+    }
 
 }
