42bfffe2193b8dc0f0d4eb7fd9441a80eebaf086
ymizushi <mizushi@gmail.com>
2020-05-18 02:07:25 +0900

    fix code

--- rust/emola/src/main.rs
+++ rust/emola/src/main.rs
@@ -6,8 +6,12 @@ mod emola {
         BracketStart,
         BracketEnd,
         Number(u32),
-        Whitespace,
         Unknown,
+    }
+
+    pub enum AST {
+        Tree(Vec<AST>),
+        Leaf(Token),
     }
 
     impl fmt::Display for Token {
@@ -18,34 +22,57 @@ mod emola {
                 Token::BracketStart => write!(f, "{}", "("),
                 Token::BracketEnd => write!(f, "{}", ")"),
                 Token::Number(i) => write!(f, "{}", i),
-                Token::Whitespace => write!(f, "{}", " "),
                 Token::Unknown => write!(f, "{}", "Unknown"),
             }
         }
+    }
+
+    pub fn parse(tokens: Vec<Token>) -> AST  {
+        let mut current_ast: AST;
+        for t in tokens {
+            let ast = match t {
+                Token::Plus => AST::Leaf(Token::Plus),
+                Token::Minus => AST::Leaf(Token::Minus),
+                Token::BracketStart => AST::Tree(vec![]),
+                Token::BracketEnd => continue,
+                Token::Number(i) => AST::Leaf(Token::Number(i)),
+                Token::Unknown => continue,
+            };
+            current_ast = ast;
+        }
+        current_ast
     }
 }
 
 
 fn main() {
-    let inputs = "(+ 1 2)";
-    {
-        let tokens: Vec<emola::Token> = vec![];
-        for c in inputs.chars() {
-            let token: emola::Token = match c {
-                '(' => emola::Token::BracketStart,
-                ')' => emola::Token::BracketEnd,
-                '+' => emola::Token::Plus,
-                '-' => emola::Token::Minus,
-                ' ' => emola::Token::Whitespace,
-                mc => {
-                    if mc.is_ascii_digit() {
-                        emola::Token::Number(mc.to_digit(10).unwrap())
-                    } else {
-                        emola::Token::Unknown
-                    }
+    let inputs = "(+ 10 20 30)";
+    let mut tokens: Vec<emola::Token> = vec![];
+    let mut current_string: String = "".to_string();
+    let replaced_inputs = inputs.replace("(", " ( ").replace(")", " ) ");
+    let chars = replaced_inputs.chars();
+    for c in chars {
+        let token: emola::Token = match c {
+            '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => {
+                current_string.push(c);
+                continue;
+            },
+            '(' => emola::Token::BracketStart,
+            ')' => emola::Token::BracketEnd,
+            '+' => emola::Token::Plus,
+            '-' => emola::Token::Minus,
+            ' ' => {
+                if current_string.len() != 0 {
+                    let num = current_string.parse().unwrap();
+                    current_string.truncate(0);
+                    emola::Token::Number(num)
+                } else {
+                    continue;
                 }
-            };
-            println!("{}", token);
-        }
+            },
+            _ => emola::Token::Unknown
+        };
+        println!("{}", token);
+        tokens.push(token);
     }
 }
