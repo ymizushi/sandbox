e6ff451dcde4e80ad31515186eea9cefff594ae5
ymizushi <mizushi@gmail.com>
2020-05-24 14:25:10 +0900

    add minigrep

---
+++ rust/the-rust-programming-language-ja/minigrep/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // {}を探しています
+    println!("Searching for {}", config.query);
+    // {}というファイルの中
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
