a6ffa8ead9e5c77fdb5e2e3c912bfd3221a43cc4
ymizushi <mizushi@gmail.com>
2020-05-27 22:25:31 +0900

    fix code

--- rust/rerd/src/main.rs
+++ rust/rerd/src/main.rs
@@ -3,6 +3,55 @@ use svg::node::element::Path;
 use svg::node::element::Rectangle;
 use svg::node::element::path::Data;
 
+
+extern crate rustc_ap_graphviz;
+use std::io::Write;
+use graphviz as dot;
+
+type Nd = isize;
+type Ed = (isize,isize);
+struct Edges(Vec<Ed>);
+
+pub fn render_to<W: Write>(output: &mut W) {
+    let edges = Edges(vec![(0,1), (0,2), (1,3), (2,3), (3,4), (4,4)]);
+    dot::render(&edges, output).unwrap()
+}
+
+impl<'a> dot::Labeller<'a> for Edges {
+    type Node = Nd;
+    type Edge = Ed;
+    fn graph_id(&'a self) -> dot::Id<'a> { dot::Id::new("example1").unwrap() }
+
+    fn node_id(&'a self, n: &Nd) -> dot::Id<'a> {
+        dot::Id::new(format!("N{}", *n)).unwrap()
+    }
+}
+
+impl<'a> dot::GraphWalk<'a> for Edges {
+    type Node = Nd;
+    type Edge = Ed;
+    fn nodes(&self) -> dot::Nodes<'a,Nd> {
+        // (assumes that |N| \approxeq |E|)
+        let &Edges(ref v) = self;
+        let mut nodes = Vec::with_capacity(v.len());
+        for &(s,t) in v {
+            nodes.push(s); nodes.push(t);
+        }
+        nodes.sort();
+        nodes.dedup();
+        nodes.into()
+    }
+
+    fn edges(&'a self) -> dot::Edges<'a,Ed> {
+        let &Edges(ref edges) = self;
+        (&edges[..]).into()
+    }
+
+    fn source(&self, e: &Ed) -> Nd { let &(s,_) = e; s }
+
+    fn target(&self, e: &Ed) -> Nd { let &(_,t) = e; t }
+}
+
 #[derive(Debug)]
 struct Table<'a> {
     name: &'a str,
@@ -39,8 +88,8 @@ impl Point {
 }
 #[derive(Debug)]
 struct Size {
-    x: i32,
-    y: i32
+    width: i32,
+    height: i32
 }
 
 
@@ -84,11 +133,16 @@ impl Canvas {
 }
 
 
-fn calc_points(tables: Vec<Table>) -> Vec<(Table, Point, 
+fn calc_points(tables: Vec<Table>) -> Vec<(Table, Point, Size)> {
+    vec![(
+        Table { name: "Person", relations: vec![] },
+        Point::new(0, 0),
+        Size { width: 10, height: 10}
+    )]
+}
 
 fn main() {
-    let company = Table { name: "Person", relations: vec![] };
-    let person = Table { name: "Company", relations: vec![&company] };
+    let company = Table { name: "Person", relations: vec![] }; let person = Table { name: "Company", relations: vec![&company] };
     let tables = vec![&company, &person];
     let canvas = Canvas::new(100, 100);
     let document = Document::new()
@@ -98,4 +152,8 @@ fn main() {
     println!("{:?}", tables);
 
     svg::save("image.svg", &returned_document).unwrap();
+
+    use std::fs::File;
+    let mut f = File::create("example1.dot").unwrap();
+    render_to(&mut f)
 }
