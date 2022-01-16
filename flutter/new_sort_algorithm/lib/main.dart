f910ca08e63729ae5b49d15271be470e23e5dbe8
ymizushi <mizushi@gmail.com>
2022-01-16 15:12:47 +0900

    new sort

--- flutter/new_sort_algorithm/lib/main.dart
+++ flutter/new_sort_algorithm/lib/main.dart
@@ -2,36 +2,27 @@ import 'package:flutter/material.dart';
 import 'extensions.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NewSortApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class NewSortApp extends StatelessWidget {
+  const NewSortApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'New sort algorithm demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const NewSortPage(title: 'New sort algorithm demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class NewSortPage extends StatefulWidget {
+  const NewSortPage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -45,15 +36,30 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<NewSortPage> createState() => _NewSortPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _NewSortPageState extends State<NewSortPage> {
   List<int> _numbers = [2, 12, 3, 12, 345, 10, 30];
 
-  void _incrementCounter() {
+  void _sort() async {
+    for (var l in _numbers.nsort()) {
+      await Future.delayed(const Duration(seconds: 1));
+      setState(() {
+        _numbers = l;
+      });
+    }
+  }
+
+  void _submitText(String va) async {
+    List<int> v = va
+        .replaceAll("[", "")
+        .replaceAll("]", "")
+        .split(",")
+        .map((v) => int.parse(v))
+        .toList();
     setState(() {
-      _numbers = _numbers.nsort();
+      _numbers = v;
     });
   }
 
@@ -91,20 +97,24 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
               '$_numbers',
               style: Theme.of(context).textTheme.headline4,
             ),
+            TextField(
+              onSubmitted: _submitText,
+              decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  hintText: _numbers.toString(),
+                  labelText: "カンマ区切りの数字を入力してね"),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _sort,
+        tooltip: 'Sort!!',
+        child: const Text("Sort"),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
