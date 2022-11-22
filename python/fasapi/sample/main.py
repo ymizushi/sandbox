ac9e11f5ae813d2322dd4bf84df36bbd67b71be1
ymizushi <mizushi@gmail.com>
2022-11-23 00:47:01 +0900

    add fastapi sample

---
+++ python/fasapi/sample/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    item = Item("hoge", 10, True)
+    print(item.name)
+    return {"item_name": item.name, "item_id": item_id}
